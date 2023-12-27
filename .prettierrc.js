8aad1f69c016dbe3e141b74787077248e39a4238
newObjectccc <897333949@qq.com>
2023-12-27 09:53:37 +0800

    chore(prettierrc.js): prettier set up

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+	arrowParens: 'always',
+	bracketSpacing: true,
+	endOfLine: 'lf',
+	htmlWhitespaceSensitivity: 'css',
+	insertPragma: false,
+	jsxBracketSameLine: false,
+	jsxSingleQuote: false,
+	printWidth: 100,
+	proseWrap: 'preserve',
+	quoteProps: 'as-needed',
+	requirePragma: false,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'none',
+	useTabs: true
+};
